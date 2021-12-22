297e2f6ecbd4269a87f500c3b5888194c74489c8
Edwin <edwingrow16@gmail.com>
2021-12-22 20:57:05 -0300

    Actualizacion del Ecomerce

--- public/js/app.js
+++ public/js/app.js
@@ -7,24 +7,24 @@ fetch('./js/productos.json')
     document.getElementById("cart-btn").innerHTML = `
       My cart ${productos.length === 0 ? '' : '(' + productos.length + ')'} <i class="fas fa-shopping-cart"></i>
     `;
-    data.forEach((producto, indice) => {
+    data.forEach((producto, index) => {
       contenedor.innerHTML += `
         <div class="product">
           <img src="${producto.img}" alt="" class="product__img">
           <div class="product__description">
-            <h3 class="product__title">${producto.nombre} ${indice + 1}</h3>
-            <span class="product__price">${producto.precio}</span>
+            <h3 class="product__title">${producto.nombre} ${index + 1}</h3>
+            <span class="product__price">$${producto.precio}</span>
           </div>
-          <button class="btnCart" id="boton${indice}" ><i class="product__icon fas fa-cart-plus"></i></button>
+          <button class="btnCart" id="boton${index}" ><i class="product__icon fas fa-cart-plus"></i></button>
         </div>
     `})
-    data.forEach((producto, indice) => {
-      document.getElementById(`boton${indice}`).addEventListener('click', function () {
+    data.forEach((producto, index) => {
+      document.getElementById(`boton${index}`).addEventListener('click', function () {
         let productos = JSON.parse(localStorage.getItem("productos")) || [];
         productos.push(producto)
         localStorage.setItem('productos', JSON.stringify(productos));
         document.getElementById("cart-btn").innerHTML = `
-          My cart (${productos.length}) <i class="fas fa-shopping-cart"></i>
+          My cart (${productos.length+1}) <i class="fas fa-shopping-cart"></i>
         `;
       })
 
